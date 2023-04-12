32a1cc83512d908e01c2b4d50f4bf3b638b64470
whosemario <gx546532416@gmail.com>
2023-04-12 14:01:56 +0800

    Update finetune.sh

--- finetune.sh
+++ finetune.sh
@@ -1,10 +1,11 @@
-python finetune.py \
+nohup python finetune.py \
     --base_model 'decapoda-research/llama-7b-hf' \
     --data_path './input/py_code_1.json' \
     --output_dir './python-lora-1'  \
-    --num_epochs=5 \
-    --cutoff_len=512 \
+    --num_epochs=3 \
+    --cutoff_len=256 \
     --group_by_length \
     --lora_target_modules='[q_proj,k_proj,v_proj,o_proj]' \
-    --lora_r=16 \
-    --micro_batch_size=8
+    --lora_r=8 \
+    --micro_batch_size=4 \
+    1>./log.txt 2>&1 &
